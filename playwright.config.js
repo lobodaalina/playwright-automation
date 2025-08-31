ad590e061817954284526105e945274f94fbac5a
Alina Loboda <lobodaalina99@gmail.com>
2025-08-31 10:15:48 +0200

    Added homework 24

--- playwright.config.js
+++ playwright.config.js
@@ -27,8 +27,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
+    baseURL: 'https://guest:welcome2qauto@qauto.forstudy.space',
+     video: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
